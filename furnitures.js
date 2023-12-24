b19a9c082a728b0a88b4812ee2edd3629123fc7f
Maestro <maestro@gmail.com>
2023-12-24 03:18:44 +0100

    working with furniture html file here

---
+++ furnitures.js
@@ -0,0 +1,37 @@
+let closer = document.querySelector('#closer');
+
+closer.onclick=()=>{
+    closer.style.display = 'none';
+    navbar.classList.remove('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick=()=>{
+    closer.style.display ='block';
+    navbar.classList.toggle('active');
+}
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick=()=>{
+    closer.style.display ='block';
+    cart.classList.toggle('active');
+}
+
+let user = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick=()=>{
+    closer.style.display ='block';
+    user.classList.toggle('active');
+}
+
+let searchForm = document.querySelector('.header .search-form');
+document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+}
+window.onscroll('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+}
